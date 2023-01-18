a8ff0ea61cb6bdc8e98e1b3f75207cc43fc6cf63
Renboou <94653387+Renboou@users.noreply.github.com>
2023-01-17 18:45:42 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function preload(){
+}
+
+function setup() {
+    canvas = createCanvas(640,480);
+    canvas.position(110,250);
+    video = createCapture(VIDEO);
+    video.hide();
+    tint_color = "";
+}
+
+function draw() {
+image(video,0,0,639,480);
+tint(tint_color);
+}
+
+function take_snapshot(){
+save('mega_cool_pic.png')
+
+}
+
+function filter_tint(){
+tint_color = document.getElementById("color_name").value;
+}
+
+
+
+
+
+
+
+
+
+
